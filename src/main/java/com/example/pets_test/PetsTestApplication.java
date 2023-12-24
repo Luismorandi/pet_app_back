fd3ed4e9c11644c08fe6c81d1f3bfb5f0b8644f9
luismo <luismmorandit@gmail.com>
2023-12-24 03:41:21 -0300

    first_commit

---
+++ src/main/java/com/example/pets_test/PetsTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.pets_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pets_test/PetsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pets_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
